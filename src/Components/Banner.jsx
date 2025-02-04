4ecae23fe41edd34f591f830fbe0e6412afed167
Cody-nas <nasiruadamu214@gmail.com>
2025-02-04 14:55:50 +0100

    Refactor EventBanner component layout for improved responsiveness and styling

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,22 +3,26 @@ import Bannerimage from "../assets/Banner.jpg"
 
 const EventBanner = () => {
   return (
-    <div className="max-w-screen-2xl md:px-12 p-8 rounded-xl shadow-lg flex flex-col lg:flex-row justify-between items-center my-12 mx-12">
-      <div>
+    <section className='flex-col md:items-center md:px-12 p-4 max-w-screen-2xl shadow-lg mx-auto my-24'>
+      <div className="flex flex-col md:flex-row justify-evenly items-center ">
+        <div>
+          <img src={Bannerimage} alt="" className="w-[450px] h-[450px]" />
+        </div>
 
+
+        <div className="mt-6 lg:mt-0">
+          <h2 className="text-2xl font-medium">  Make your own Event</h2>
+          <p className="mt-2 text-gray-300 max-w-2xl ">
+            Lorem ipsum dolor sit amet consectetur. Blandit odio semper risus
+            pellentesque elit. Pellentesque eget ut imperdiet nulla penatibus.
+            Nascetur viverra arcu sed amet cursus purus.
+          </p>
+          <button className="mt-6 px-8 py-3 cursor-pointer bg-blue-600 text-white font-semibold rounded-full shadow-lg hover:bg-blue-700 transition-all">
+            Explore Events
+          </button>
+        </div>
       </div>
-      <div className="mt-6 lg:mt-0">
-        <h2 className="text-2xl font-medium">  Make your own Event</h2>
-        <p className="mt-2 text-gray-300 max-w-2xl ">
-          Lorem ipsum dolor sit amet consectetur. Blandit odio semper risus
-          pellentesque elit. Pellentesque eget ut imperdiet nulla penatibus.
-          Nascetur viverra arcu sed amet cursus purus.
-        </p>
-        <button className="mt-6 px-8 py-3 cursor-pointer bg-blue-600 text-white font-semibold rounded-full shadow-lg hover:bg-blue-700 transition-all">
-          Explore Events
-        </button>
-      </div>
-    </div>
+    </section>
   );
 };
 
